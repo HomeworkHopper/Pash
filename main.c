36932277231c002a146520e830fab6eb3ccb59c7
Shaun D Thornton <sdt8987@cs.rit.edu>
2022-05-19 23:09:59 -0400

    Fixed memory leak in main module Committer: Shaun D Thornton <sdt8987@cs.rit.edu> On branch master modified:   main.c

--- main.c
+++ main.c
@@ -3,11 +3,9 @@
 
 #include "pash.h"
 
-#define N 1000000
+#define N 10    // The number of integers to pair
 
 int main() {
-
-    setbuf(stdout, NULL);
 
     // Starting array of integers
     mpz_t* in = malloc(sizeof(mpz_t) * N);
@@ -15,42 +13,36 @@ int main() {
     // Initialize and print each integer
     for(int i = 0; i < N; ++i) {
         mpz_init_set_ui(in[i], i + 1);
-//        gmp_printf("%Zd, ", in[i]);
+        gmp_printf("%Zd, ", in[i]);
     }
 
     // Declare and initialize the paired output
     mpz_t z;
     mpz_init(z);
 
-    fprintf(stdout, "Starting pair... ");
-
     // Pair the array of integers into a single integer (z)
     pair(z, N, in);
 
-    fprintf(stdout, "Done\n");
-
     // Print the generated integer
-  //  gmp_printf("\n%Zd\n", z);
+    gmp_printf("\n%Zd\n", z);
 
-    // Declare and initialize the array to split z back into
     mpz_t out[N];
     for(int i = 0; i < N; ++i) {
         mpz_init(out[i]);
         mpz_clear(in[i]);
     }
-    
-    fprintf(stdout, "Starting unpair... ");
 
     // Split z back into the original integers
     unpair(N, out, z);
-
-    fprintf(stdout, "Done\n");
 
     mpz_clear(z);
 
     // Print the split integers
     for(int i = 0; i < N; ++i) {
-    //    gmp_printf("%Zd, ", out[i]);
+        gmp_printf("%Zd, ", out[i]);
         mpz_clear(out[i]);
     }
+    printf("\n");
+
+    free(in);
 }
