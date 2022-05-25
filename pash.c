64b4e3e03c1fe09e72aaf1fd9fbee2448ff451b2
Shaun D Thornton <sdt8987@cs.rit.edu>
2022-05-25 10:52:59 -0400

    Fixed bug in unpair function Committer: Shaun D Thornton <sdt8987@cs.rit.edu> On branch main modified:   pash.c

--- pash.c
+++ pash.c
@@ -22,67 +22,78 @@
         mpz_sub(y, y, x); \
     }
 
-// WARNING: This function destroys the provided array
 static void pair_internal(mpz_t z, const size_t n, mpz_t x[n]) {
     
-    const size_t p = (n + 1) >> 1;  // Number of pairs
+    // Number of groups of two
+    const size_t p = (n + 1) >> 1;
 
+    // Pair each group of two
     for(size_t i = 0, j = 0; i < p - 1; ++i, j += 2) {
         PAIR(z, x[j], x[j + 1]);
         mpz_swap(x[i], z);
     }
 
+    // Take care of the last group
     if(!(n & 1)) {
         PAIR(z, x[n - 2], x[n - 1]);
-        if(p == 1) 
-            return;
-        mpz_swap(x[p - 1], z);
-    } else
-        mpz_swap(x[p - 1], x[n - 1]);
+    } else {
+        mpz_swap(z, x[n - 1]);
+    }
 
-    pair_internal(z, p, x);  // Recurse
+    // If there was only one group, we're done
+    if(p == 1) {
+        return;
+    }
+
+    // Swap z back into the array
+    mpz_swap(x[p - 1], z);
+
+    // Recurse
+    pair_internal(z, p, x);
 }
 
 static void unpair_internal(const size_t n, mpz_t res[n], const mpz_t z) {
 
+    // Base case #1
     if(n == 1) {
         mpz_set(res[0], z);
         return;
     }
 
+    // Base case #2
     if(n == 2) {
         UNPAIR(res[0], res[1], z);
         return;
     }
 
-    // Local vars
+    // Result variables
     mpz_t x, y;
     mpz_init(x);
     mpz_init(y);
 
-    // Split
+    // Unpair z into x and y
     UNPAIR(x, y, z);
 
-    // Is perfect power of two?
+    // If n is a perfect power of two...
     if(!(n & (n - 1))) {
 
-        // n / 2
+        // 0.5 * n
         const size_t mid = n >> 1;
 
-        // Unpair left
+        // Unpair x
         unpair_internal(mid, res, x);
 
-        // Unpair right
+        // Unpair y
         unpair_internal(mid, res + mid, y);
     } else {
 
-        // 10000000000000000000000000000000 >> number of leading zeros in n
+        // Nearest power of two <= n
         const size_t npt = 0x80000000 >> __builtin_clz(n);
 
-        // Unpair left
+        // Unpair x
         unpair_internal(npt, res, x);
 
-        // Unpair right
+        // Unpair y
         unpair_internal(n - npt, res + npt, y);
     }
 
